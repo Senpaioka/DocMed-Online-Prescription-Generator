34830f36a6dec98f0aecd2b713e39ff1b85b46a9
Abdul Mannan <abdul.gameprofile01@gmail.com>
2025-02-26 14:48:09 +0600

    vercel config

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
+from serverless_wsgi import handle_request
 from app.app import create_app
+
+
 
 flask_app = create_app()
 
 # Vercel needs this handler for deployment
-def handler(event, context):
-    return flask_app
+def vercel_handler(event, context):
+    return handle_request(flask_app, event, context)
+
+
 
 if __name__ == '__main__':
-    flask_app.run()
+    flask_app.run(debug=True)
