76de8c9b65e35c403f865f941156182c19e9a8c9
Abdul Mannan <abdul.gameprofile01@gmail.com>
2025-02-26 17:54:01 +0600

    few config update

--- manage.py
+++ manage.py
@@ -1,13 +1,7 @@
-from serverless_wsgi import handle_request
 from app.app import create_app
 
-# Create the Flask application
 flask_app = create_app()
 
-# Vercel needs this handler function
-def vercel_handler(event, context):
-    return handle_request(flask_app, event, context)
 
-# Run Flask app locally for testing
 if __name__ == '__main__':
     flask_app.run()
