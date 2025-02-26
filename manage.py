0f17d47d01f9e23bd1f1012499febfda3cf3e953
Abdul Mannan <abdul.gameprofile01@gmail.com>
2025-02-26 15:37:09 +0600

    fixes

--- manage.py
+++ manage.py
@@ -10,11 +10,12 @@ flask_app = create_app()
 
 class handler(BaseHTTPRequestHandler):
 
+
     # Vercel needs this handler for deployment
-    def vercel_handler(event, context):
-        return handle_request(flask_app, event, context)
+    # def vercel_handler(event, context):
+    #     return handle_request(flask_app, event, context)
 
 
 
-if __name__ == '__main__':
-    flask_app.run()
+    if __name__ == '__main__':
+        flask_app.run()
