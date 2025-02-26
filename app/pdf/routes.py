4d5141eff485235080644b5ab1dc1e46ed5e34d9
Abdul Mannan <abdul.gameprofile01@gmail.com>
2025-02-26 12:28:58 +0600

    pdf functionality added

--- app/pdf/routes.py
+++ app/pdf/routes.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, redirect, url_for, flash, Response, Blueprint, current_app
+from flask import request, render_template, redirect, url_for, flash, Response, Blueprint, current_app, make_response
 from app.app import db
 from sqlalchemy.exc import IntegrityError
 from flask_login import login_required, current_user
@@ -6,6 +6,9 @@ from app.pdf.models import PrescriptionModel
 from app.account.models import RegistrationModel
 from app.pdf.forms import PrescriptionForm
 import uuid
+from xhtml2pdf import pisa
+import io
+import os
 
 
 
@@ -102,12 +105,65 @@ def pdf_prescription_preview(patient_id):
 
 
 
+# Function to convert HTML to PDF and return it as response
+from flask import render_template, make_response, current_app
+from xhtml2pdf import pisa
+import io
+import os
+
+def create_pdf(template_name, context):
+    html = render_template(template_name, **context)
+    pdf = io.BytesIO()
+    pisa_status = pisa.CreatePDF(
+        html, dest=pdf, link_callback=lambda uri, rel: os.path.join(current_app.root_path, uri.lstrip('/'))
+    )
+
+    if pisa_status.err:
+        return "Error generating PDF", 500
+
+    pdf.seek(0)
+    response = make_response(pdf.read())
+    response.headers['Content-Type'] = 'application/pdf'
+    response.headers['Content-Disposition'] = 'inline; filename=prescription.pdf'
+    return response
+
 
 
 @pdf_generator.route('/generate_pdf/<uid>/<patient_id>')
 def pdf_generator_page(uid, patient_id):
 
-    pass
+    # getting patient prescription
+    get_patient = PrescriptionModel.query.filter_by(patient_id=patient_id).first()
+
+    # getting doctor info
+    get_doctor = RegistrationModel.query.get(uid)
+
+    context = {
+        'patient': get_patient,
+        'doctor': get_doctor,
+    }
+    # return render_template('pdf/pdf.html', **context)
+
+    return create_pdf('pdf/pdf.html', context)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # # getting patient prescription
     # get_patient = PrescriptionModel.query.filter_by(patient_id=patient_id).first()
